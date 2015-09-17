8f822101ddb22593df65a3c3aa54852bd4b1d811
Dave Hackett <tallguy.hackett@gmail.com>
2015-09-17 12:05:14 -0600

    Add validation for presence of Kaizen text

--- app/models/kaizen.rb
+++ app/models/kaizen.rb
@@ -1,4 +1,5 @@
 class Kaizen < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :user
+  validates :text, presence: true
 end
--- test/models/kaizen_test.rb
+++ test/models/kaizen_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class KaizenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "lack of kaizen text is invalid" do
+    kaizen = Kaizen.new
+    assert kaizen.invalid?
+  end
+
+  test "presence of kaizen text is valid" do
+    kaizen = Kaizen.new(text: "I love kaizens")
+    assert kaizen.valid?
+  end
 end
