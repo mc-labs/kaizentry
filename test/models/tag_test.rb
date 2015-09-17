e002e323c9fb1e9a638fbb1d30da17cf9ef3976f
Dave Hackett <tallguy.hackett@gmail.com>
2015-09-17 07:11:00 -0600

    Add test for Tag model and some validation

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "presenece of tag name" do
+    tag = Tag.new
+    assert tag.invalid?
+  end
+
+  test "uniqueness of tag name" do
+    tag = Tag.new(name: "windows")
+    assert tag.invalid?
+    tag.name = "linux"
+    assert tag.valid?
+  end
 end
