622ec36d4d04d0ac39233172dfbbc32a1b1757d8
Dave Hackett <tallguy.hackett@gmail.com>
2015-09-17 11:45:58 -0600

    Add views for the User and change the routes for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update, :show]
+
   def index
+    @users = User.all
   end
-  def create
-    @kaizen = Kaizen.find(params[:kaizen_id])
-    
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was updated successfully'
+    else
+      render :edit
+    end
   end
+
+  def show
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :kaizens do
-    resources :users
     resources :tags
   end
+  resources :users, except: [:destroy, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
