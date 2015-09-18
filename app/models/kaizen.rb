8e14ed87d0209142f5dcb6b6a044df81c9beb5d3
Dave Hackett <tallguy.hackett@gmail.com>
2015-09-17 23:03:52 -0600

    Fix creating a Kaizen with a brand new User

--- app/models/kaizen.rb
+++ app/models/kaizen.rb
@@ -21,17 +21,17 @@ class Kaizen < ActiveRecord::Base
   end
 
   def user_email=(new_value)
-    if new_value.empty?
+    if new_value.strip.empty?
       new_email = "anonymous"
     else
       new_email = new_value
     end
-    self.user = User.where('email = ?', new_email).first or User.create(email: new_email)
+    self.user = (User.where('email = ?', new_email).first or User.create(email: new_email))
   end
 
   def update_user_email=(new_value)
-    current_email = self.user.email
-    if current_email.nil?
+    current_user = self.user
+    if current_user.nil?
       self.user = User.create(email: new_value)
     end
   end
