81e985093c17e9cd04bd294f96e13e3021702e04
Dustin Dixon <bob_deus@yahoo.com>
2015-09-18 00:33:53 -0600

    Fixed issue #4

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :kaizens
   validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+  before_save { |tag| tag.name = tag.name.downcase }
 end
